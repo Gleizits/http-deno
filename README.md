1ba00f86d86040f233a9283959b149f03e9986d0
Geissel Espino <geisselespino05@outlook.com>
2022-06-03 21:30:42 -0500

    Add files via upload
    This additional files.

---
+++ README.md
@@ -0,0 +1,19 @@
+---
+marp: true
+---
+## This is a hello word in Deno using http.
+
+<style>
+    .heatMap {
+        width: 70%;
+        text-align: center;
+    }
+    .heatMap th {
+        background: grey;
+        word-wrap: break-word;
+        text-align: center;
+    }
+    .heatMap tr:nth-child(1) { background: red; }
+    .heatMap tr:nth-child(2) { background: orange; }
+    .heatMap tr:nth-child(3) { background: green; }
+</style>
---
+++ app.js
@@ -0,0 +1,14 @@
+import {Application} from 'https://deno.land/x/oak@v6.0.0/mod.ts';
+import {
+  viewEngine,
+  engineFactory,
+  adapterFactory
+} from 'https://deno.land/x/view_engine@v1.3.0/mod.ts';
+const ejsEngine = engineFactory.getEjsEngine();
+const oakAdapter = adapterFactory.getOakAdapter();
+const app = new Application();
+app.use(viewEngine(oakAdapter, ejsEngine));
+app.use(async (ctx, next) => {
+  ctx.render('index.ejs', {data: {msg: 'Tips'}});
+});
+await app.listen({port: 8000});
